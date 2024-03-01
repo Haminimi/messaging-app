7198f5780e88a32aeeb1e398ee7855d6a89971f8
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-03-02 00:29:03 +0100

    Add the App component

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Outlet, useNavigation } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import AuthProvider from './context/AuthProvider';
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'loaders.css/loaders.min.css';
+
+function App() {
+	const navigation = useNavigation();
+
+	return (
+		<AuthProvider>
+			<div className="main-container">
+				<Sidebar />
+				<div
+					id="outlet-container"
+					className={
+						navigation.state === 'loading' ? 'outlet-loading' : ''
+					}
+				>
+					<Outlet />
+				</div>
+			</div>
+		</AuthProvider>
+	);
+}
+
+export default App;
