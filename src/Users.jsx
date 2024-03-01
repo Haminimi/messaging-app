40310e276a8fd2b8eb9f14061de5c34d1f50dfc3
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-03-02 00:20:43 +0100

    Add the Users component

---
+++ src/Users.jsx
@@ -0,0 +1,64 @@
+import { Link, useLoaderData } from 'react-router-dom';
+import { useAuth } from './context/AuthProvider';
+import Loader from 'react-loaders';
+
+export async function loader() {
+	const token = localStorage?.getItem('token');
+	if (token !== null) {
+		const response = await fetch(
+			`${import.meta.env.VITE_BACKEND_URL}/users`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		const data = await response.json();
+		const users = data.users;
+		return { users };
+	} else {
+		const users = [];
+		return { users };
+	}
+}
+
+function Users() {
+	const auth = useAuth();
+	const user = auth.user;
+	const { users } = useLoaderData();
+
+	return (
+		<div className="content-container">
+			{user ? (
+				<div className="users-container">
+					<div className="users">
+						{users.length > 0 ? (
+							users.map((user) => (
+								<Link key={user._id} to={`/users/${user._id}`}>
+									<div className="users-list-user">
+										<img
+											src={`${
+												import.meta.env.VITE_BACKEND_URL
+											}/${user.avatar}`}
+											alt=""
+											className="chats-avatar"
+										/>
+										<p>
+											{user.firstName} {user.lastName}
+										</p>
+									</div>
+								</Link>
+							))
+						) : (
+							<p>There are no users.</p>
+						)}
+					</div>
+				</div>
+			) : (
+				<Loader type="line-scale" active />
+			)}
+		</div>
+	);
+}
+
+export default Users;
